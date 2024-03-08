fd7e80ece9ea73f454294876a90df73a05cfa47f
dimanu-py <diegomtz126@gmail.com>
2024-03-08 17:51:42 +0100

    refactor: leave only configuration on normal startup

--- setup.sh
+++ setup.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
 
-paru -S -y evdi displaylink
-echo "Installed evdi and displaylink successfully"
+main_monitor="DVI-I-2-1"
+secondary_monitor="DVI-I-3-2"
+internal_monitor="eDP-1"
 
-systemctl enable displaylink.service
-echo "Displaylink enable"
+xrandr --output $main_monitor --mode 1920x1080 --same-as $internal_monitor
+echo "Set $main_monitor as mirror of internal monitor"
 
-file_path="/etc/X11/xorg.conf.d/20-evdi.conf"
-
-if [ ! -d "$(dirname "$file_path")"  ]; then
-	sudo mkdir -p "$(dirname "$file_path")"
-fi
-
-sudo tee "$file_path" > /dev/null << EOF
-Section "OutputClass"
-	Identifier "DisplayLink"
-	MatchDriver "evdi"
-	Driver "modesetting"
-	Option "AccelMethod" "none"
-EndSection
-EOF
-
-echo "Configuration written to $file_path"
-
-xrandr --setprovideroutputsource 1 0
-xrandr --setprovideroutputsource 2 0
-
-xrandr --output DVI-I-2-1 --mode 1920x1080 --same-as eDP-1
-xrandr --output DVI-I-3-2 --mode 1920x1080 --right-of DVI-I-2-1
+xrandr --output $secondary_monitor --mode 1920x1080 --right-of $main_monitor
+echo "Set $secondary_monitor as monitor on the right of $main_monitor main monitor"
