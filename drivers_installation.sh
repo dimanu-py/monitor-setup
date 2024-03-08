d16a805ff7e2a622d03c4c8a8b0540eb90a83d90
dimanu-py <diegomtz126@gmail.com>
2024-03-08 17:51:15 +0100

    refactor(driver-installation): separate installation steps into a single script
    This way we can execute this commands just once and leave the other script to be run on startup

---
+++ drivers_installation.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+paru -S -y evdi displaylink
+echo "Installed evdi and displaylink successfully"
+
+systemctl enable displaylink.service
+echo "Displaylink enable"
+
+file_path="/etc/X11/xorg.conf.d/20-evdi.conf"
+
+if [ ! -d "$(dirname "$file_path")"  ]; then
+	sudo mkdir -p "$(dirname "$file_path")"
+fi
+
+sudo tee "$file_path" > /dev/null << EOF
+Section "OutputClass"
+	Identifier "DisplayLink"
+	MatchDriver "evdi"
+	Driver "modesetting"
+	Option "AccelMethod" "none"
+EndSection
+EOF
+echo "Configuration written to $file_path"
+
+xrandr --setprovideroutputsource 1 0
+xrandr --setprovideroutputsource 2 0
+echo "Set outputs source correctly" 
