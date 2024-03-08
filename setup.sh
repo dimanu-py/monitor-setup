5fcdf021c2f00b06179f5ac9d662bdd53e0e6dc4
dimanu-py <diegomtz126@gmail.com>
2024-03-08 17:43:23 +0100

    feat: add complete configuration, installing evdi, displaylink and setting up the outputs

--- setup.sh
+++ setup.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
 
-xrandr --output DVI-I-3-2 --mode auto --same-as eDP-1
-xranr --output DVI-I-2-1 --mode auto --right-of DVI-I-3-2
+paru -S -y evdi displaylink
+echo "Installed evdi and displaylink successfully"
+
+systemctl enable displaylink.service
+echo "Displaylink enable"
+
+file_path="/etc/X11/xorg.conf.d/20-evdi.conf"
+
+if [ ! -d "$(dirname "$file_path")"  ]; then
+	sudo mkdir -p "$(dirname "$file_path")"
+fi
+
+sudo tee "$file_path" > /dev/null << EOF
+Section "OutputClass"
+	Identifier "DisplayLink"
+	MatchDriver "evdi"
+	Driver "modesetting"
+	Option "AccelMethod" "none"
+EndSection
+EOF
+
+echo "Configuration written to $file_path"
+
+xrandr --setprovideroutputsource 1 0
+xrandr --setprovideroutputsource 2 0
+
+xrandr --output DVI-I-2-1 --mode 1920x1080 --same-as eDP-1
+xrandr --output DVI-I-3-2 --mode 1920x1080 --right-of DVI-I-2-1
