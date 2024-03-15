7a1421dfdc32baa84dd9b609fe9acdbdcf1e9cc3
dimanu-py <diegomtz126@gmail.com>
2024-03-15 20:52:11 +0100

    refactor: allow to pass custom monitor names to script and use default values if not
    If no arguments are passed, my default value will be used

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 #!/bin/zsh
 
-main_monitor="DVI-I-2-1"
-secondary_monitor="DVI-I-3-2"
-internal_monitor="eDP-1"
+default_internal_monitor="eDP-1"
+default_main_monitor="DVI-I-2-1"
+default_secondary_monitor="DVI-I-3-2"
+
+internal_monitor=${1:-$default_internal_monitor}
+main_monitor=${2:-$default_main_monitor}
+secondary_monitor=${3:-$default_secondary_monitor}
 
 xrandr --output $internal_monitor --same-as $main_monitor --mode 1920x1080
 echo "Set $main_monitor as mirror of internal monitor"
