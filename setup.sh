5b0f2a8dbfc7bb8418d13ea4539b9463e4f94162
dimanu-py <diegomtz126@gmail.com>
2024-03-15 20:40:48 +0100

    refactor: modify order of monitors when mirroring laptop monitor

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ main_monitor="DVI-I-2-1"
 secondary_monitor="DVI-I-3-2"
 internal_monitor="eDP-1"
 
-xrandr --output $main_monitor --mode 1920x1080 --same-as $internal_monitor
+xrandr --output $internal_monitor --same-as $main_monitor --mode 1920x1080
 echo "Set $main_monitor as mirror of internal monitor"
 
 xrandr --output $secondary_monitor --mode 1920x1080 --right-of $main_monitor
