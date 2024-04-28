91d5ca99e9439b9a3460bd736c7dd33ebf5ae421
Diego Martinez <diegomtz126@gmail.com>
2024-04-28 19:00:57 +0200

    refactor(setup): automate external monitors name detection and allow more than two using loops

--- setup.sh
+++ setup.sh
@@ -1,15 +1,25 @@
-#!/bin/zsh
+#!/bin/bash
 
 default_internal_monitor="eDP-1"
-default_main_monitor="DVI-I-2-1"
-default_secondary_monitor="DVI-I-3-2"
+default_display_name="DVI"
 
-internal_monitor=${1:-$default_internal_monitor}
-main_monitor=${2:-$default_main_monitor}
-secondary_monitor=${3:-$default_secondary_monitor}
+display_name=${1:-$default_display_name}
+internal_monitor=${2:-$default_internal_monitor}
 
-xrandr --output $internal_monitor --same-as $main_monitor --mode 1920x1080
-echo "Set $main_monitor as mirror of internal monitor"
+mapfile -t external_monitors < <(xrandr | grep ' connected' | grep "$display_name" | awk '{print $1}')
 
-xrandr --output $secondary_monitor --mode 1920x1080 --right-of $main_monitor
-echo "Set $secondary_monitor as monitor on the right of $main_monitor main monitor"
+if [ ${#external_monitors[@]} -eq 0 ]; then
+	echo "No monitors found matching: $display_name"
+	exit 1
+fi
+
+for i in "${!external_monitors[@]}"; do
+	index=$((i + 1))
+	declare "monitor_$index=${external_monitors[i]}"
+done
+
+xrandr --output "$monitor_1" --primary --mode 1920x1080 --pos 1920x0 --rotate normal
+echo "Set $monitor_1 as mirror of internal monitor"
+
+xrandr --output "$monitor_2" --mode 1920x1080 --pos 0x0
+echo "Set $monitor_2 as monitor on the right of $monitor_1 main monitor"
